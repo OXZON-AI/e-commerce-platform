e03e0586f459723374a2fedbd43effcda62c70c4
IT21278280 <99583379+IT21278280@users.noreply.github.com>
2025-01-14 03:52:24 +0530

    Add Project Files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { Suspense, lazy } from "react";
+import ScrollToTop from "./helpers/scroll-top";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+const LoginRegister = lazy(() => import("./pages/other/LoginRegister"));
+
+
 
 const App = () => {
-  return <h1>Welcome To E-commerce Platform</h1>;
+  return (
+      <Router>
+        <ScrollToTop>
+          <Suspense
+            fallback={
+              <div className="flone-preloader-wrapper">
+                <div className="flone-preloader">
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+            }
+          >
+            <Routes>
+              
+              <Route
+                path={process.env.PUBLIC_URL + "/login-register"}
+                element={<LoginRegister/>}
+              />
+
+              
+            </Routes>
+          </Suspense>
+        </ScrollToTop>
+      </Router>
+  );
 };
 
 export default App;
