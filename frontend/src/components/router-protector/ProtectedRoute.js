33527835604be7d9710029631d3814057c77fb4b
yasirupanduka99 <22panduka@gmail.com>
2025-01-22 21:27:52 +0530

    route protector implement inside App.js

--- frontend/src/components/router-protector/ProtectedRoute.js
+++ frontend/src/components/router-protector/ProtectedRoute.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const { userInfo } = useSelector((state) => state.user); // Get user info from Redux state
 
-  return userInfo ? children : <Navigate to="/login-register" replace />;
+  // Redirect to login page if userInfo is null
+  if (!userInfo) {
+    return <Navigate to="/login-register" replace />;
+  }
+
+  // Render the protected component if userInfo exists
+  return children;
 };
 
 export default ProtectedRoute;
