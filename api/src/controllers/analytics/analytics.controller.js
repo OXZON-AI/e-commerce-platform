2263e65f5c720cafa4a5b2a2929fc04cea72a50b
prb-dev <prabashwara.in@gmail.com>
2025-01-30 10:24:32 +0530

    Create sales performance feature

--- api/src/controllers/analytics/analytics.controller.js
+++ api/src/controllers/analytics/analytics.controller.js
@@ -1,6 +1,10 @@
 import { Order } from "../../models/order.model.js";
 import { logger } from "../../utils/logger.util.js";
-import { validateSalesSummary } from "../../utils/validator.util.js";
+import {
+  validateSalesPerformance,
+  validateSalesSummary,
+} from "../../utils/validator.util.js";
+import { performancePipeline } from "./pipelines/sales/performance.pipline.js";
 import { summaryPipeline } from "./pipelines/sales/summary.pipeline.js";
 
 export const salesSummary = async (req, res, next) => {
@@ -31,7 +35,70 @@ export const salesSummary = async (req, res, next) => {
     logger.info("Sales summary fetched successfully.");
     return res.status(200).json(summary[0]);
   } catch (error) {
-    logger.error("Failed to fetch sales analytics: ", error);
+    logger.error("Failed to fetch sales summary analytics: ", error);
+    return next(error);
+  }
+};
+
+export const salesPerformance = async (req, res, next) => {
+  const { error, value } = validateSalesPerformance(req.query);
+
+  if (error) return next(error);
+
+  const { userType, status, interval, startDate, endDate } = value;
+
+  const pipeline = [...performancePipeline];
+
+  pipeline.unshift({
+    $match: {
+      ...(userType && { isGuest: userType === "customer" ? false : true }),
+      ...(status && { status }),
+      ...(startDate && {
+        createdAt: {
+          $gte: new Date(startDate),
+          ...(endDate && { $lt: new Date(endDate) }),
+        },
+      }),
+    },
+  });
+
+  pipeline.splice(4, 0, {
+    $group: {
+      _id: {
+        $dateToString: {
+          format:
+            interval === "year"
+              ? "%Y-%m-%d"
+              : interval === "month"
+              ? "%Y-%m"
+              : interval === "week"
+              ? "%Y-%U"
+              : "%Y-%m-%d",
+          date: "$createdAt",
+        },
+      },
+      orderCount: {
+        $sum: 1,
+      },
+      revenue: {
+        $sum: "$payment.amount",
+      },
+      cost: {
+        $sum: "$cost",
+      },
+      averageOrderValue: {
+        $avg: "$payment.amount",
+      },
+    },
+  });
+
+  try {
+    const performance = await Order.aggregate(pipeline);
+
+    logger.info("Sales performance fetched successfully.");
+    return res.status(200).json(performance);
+  } catch (error) {
+    logger.error("Failed to fetch sales performance analytics: ", error);
     return next(error);
   }
 };
--- api/src/routes/v1/analytics.route.js
+++ api/src/routes/v1/analytics.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { verifyToken } from "../../middleware/verifyToken.middleware.js";
 import { verifyAdmin } from "../../middleware/verifyAdmin.middleware.js";
-import { salesSummary } from "../../controllers/analytics/analytics.controller.js";
+import {
+  salesPerformance,
+  salesSummary,
+} from "../../controllers/analytics/analytics.controller.js";
 
 const route = express.Router();
 
 route.get("/sales/summary", verifyToken, verifyAdmin, salesSummary);
+route.get("/sales/performance", verifyToken, verifyAdmin, salesPerformance);
 
 export default route;
