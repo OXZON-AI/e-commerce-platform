f061c41734a675ac8411f38ebb1142267b2d83a4
yasirupanduka99 <22panduka@gmail.com>
2025-02-18 09:34:36 +0530

    "menu cart item state not  correctly fetch" bug fixed!

--- frontend/src/components/header/sub-components/MenuCart.js
+++ frontend/src/components/header/sub-components/MenuCart.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 // import { getDiscountPrice } from "../../../helpers/product";
