71f0329011c80215f255fc6285adec79b65e9bb8
yasirupanduka99 <22panduka@gmail.com>
2025-03-01 02:39:35 +0530

    stabalized admin order management table [Still Under dev]

--- frontend/src/pages/admin/AdminOrderManagement.js
+++ frontend/src/pages/admin/AdminOrderManagement.js
@@ -78,6 +78,27 @@ const AdminOrderManagement = () => {
     }));
   };
 
+  // function for change distinct colors for diffrent order status
+  const getStatusBadge = (status) => {
+    const statusStyles = {
+      pending: "border-amber-500 bg-amber-50 text-amber-600",
+      cancelled: "border-red-500 bg-red-100 text-red-600",
+      shipped: "border-blue-500 bg-blue-100 text-blue-600",
+      delivered: "border-green-500 bg-green-100 text-green-600",
+      processing: "border-purple-500 bg-purple-100 text-purple-600",
+    };
+
+    return (
+      <div
+        className={`border-1 px-2 py-1 rounded font-normal ${
+          statusStyles[status] || "border-gray-500 bg-gray-100 text-gray-600"
+        }`}
+      >
+        {status}
+      </div>
+    );
+  };
+
   return (
     <div className="flex flex-col h-screen">
       {/* Navbar on Top */}
@@ -152,6 +173,7 @@ const AdminOrderManagement = () => {
               <table className="min-w-full table-auto bg-white border border-gray-200 rounded-lg shadow-sm mt-6">
                 <thead className="bg-gray-100 text-base">
                   <tr>
+                    <th className="p-4 text-left">Index</th>
                     <th className="p-4 text-left">Order ID</th>
                     <th className="p-4 text-left">Delivery Date</th>
                     <th className="p-4 text-left">Time Slot</th>
@@ -164,31 +186,43 @@ const AdminOrderManagement = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {filteredOrders.map((order) => (
-                    <tr
-                      key={order.orderId}
-                      className="border-b hover:bg-gray-50"
-                    >
-                      <td className="p-4">{order.orderId}</td>
-                      <td className="p-4">{order.deliveryDate}</td>
-                      <td className="p-4">{order.timeSlot}</td>
-                      <td className="p-4">{order.customerName}</td>
-                      <td className="p-4">{order.branch}</td>
-                      <td className="p-4">{order.totalAmount}</td>
-                      <td className="p-4">{order.status}</td>
-                      <td className="p-4">{order.orderType}</td>
-                      <td className="p-4">
-                        <Link to="/manage-order-details">
-                          <button className="text-blue-500 hover:underline mr-4">
-                            <FaEye className="mr-2" size={24} />
-                          </button>
-                        </Link>
-                        <button className="text-yellow-500 hover:underline">
-                          <FaEdit className="mr-2" size={24} />
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
+                  {orders &&
+                    orders.map((order, index) => {
+                      // Calculate the starting index based on the current page
+                      const currentIndex =
+                        (filters.page - 1) * filters.limit + index + 1;
+
+                      return (
+                        <tr
+                          key={order.orderId}
+                          className="border-b hover:bg-gray-50"
+                        >
+                          <td className="py-4 px-6 text-gray-800">
+                            {currentIndex}
+                          </td>
+                          <td className="p-4">{order._id}</td>
+                          <td className="p-4">{order.deliveryDate}</td>
+                          <td className="p-4">{order.timeSlot}</td>
+                          <td className="p-4">{order.user}</td>
+                          <td className="p-4">{order.branch}</td>
+                          <td className="p-4">{order.payment.amount}</td>
+                          <td className="p-3">
+                            {getStatusBadge(order.status)}
+                          </td>
+                          <td className="p-4">{order.orderType}</td>
+                          <td className="p-4">
+                            <Link to="/manage-order-details">
+                              <button className="text-blue-500 hover:underline mr-4">
+                                <FaEye className="mr-2" size={24} />
+                              </button>
+                            </Link>
+                            <button className="text-yellow-500 hover:underline">
+                              <FaEdit className="mr-2" size={24} />
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })}
                 </tbody>
               </table>
 
