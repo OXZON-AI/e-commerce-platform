a5deef6fda0a730cc042f5952c3ca98e1a0e6dee
yasirupanduka99 <22panduka@gmail.com>
2025-03-03 20:06:05 +0530

    create review state handling cases in review-slice

--- frontend/src/store/slices/review-slice.js
+++ frontend/src/store/slices/review-slice.js
@@ -4,9 +4,9 @@ import axiosInstance from "../../axiosConfig";
 // Async thunk for fetching reviews
 export const fetchReviews = createAsyncThunk(
   "reviews/fetchReviews",
-  async (slug, { rejectWithValue }) => {
+  async (productSlug, { rejectWithValue }) => {
     try {
-      const response = await axiosInstance.get(`/v1/reviews/${slug}`);
+      const response = await axiosInstance.get(`/v1/reviews/${productSlug}`);
       console.log("Fetch-reviews-slice : ", response.data);
       return response.data;
     } catch (error) {
@@ -20,11 +20,12 @@ export const fetchReviews = createAsyncThunk(
 // Async thunk for creating a review
 export const createReview = createAsyncThunk(
   "reviews/createReview",
-  async (reviewData, { rejectWithValue }) => {
+  async (reviewData, { dispatch, rejectWithValue }) => {
     try {
       const response = await axiosInstance.post(`/v1/reviews/`, reviewData);
       console.log("Post-review-slice : ", response.data);
-      return response.data;
+      
+      dispatch(fetchReviews(reviewData.slug)); // Refetch reviews to update.
     } catch (error) {
       return rejectWithValue(
         error.response?.data?.message || "Failed to create review!"
@@ -32,3 +33,45 @@ export const createReview = createAsyncThunk(
     }
   }
 );
+
+const reviewSlice = createSlice({
+  name: "reviews",
+  initialState: {
+    reviews: [],
+    counts: {},
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // handling states for fetch reviews
+      .addCase(fetchReviews.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchReviews.fulfilled, (state, action) => {
+        state.loading = false;
+        state.reviews = action.payload.reviews;
+        state.counts = action.payload.counts;
+      })
+      .addCase(fetchReviews.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      // handling states for create review
+      .addCase(createReview.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createReview.fulfilled, (state, action) => {
+        state.loading = false;
+      })
+      .addCase(createReview.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default reviewSlice.reducer;
